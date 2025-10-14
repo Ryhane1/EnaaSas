ec36c572c9d9ee3e5420e23649452996dd69caa2
RYHANE <Ryhane.Harchi@gmail.com>
2025-10-14 15:01:34 +0100

    Initial test

---
+++ test.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+let Nom = prompt("nom ");
+let Prenom = prompt("Prenom ");
+let sex = prompt("sexe ");
+let Age = prompt("Age ");
+let Telephone = prompt("Telephone ")
+
+
+console.log("Nom:"+Nom);
+console.log("prenom:"+Prenom);
+console.log("Sex:"+sex);
+console.log("Age:"+Age);
+console.log("Telephone:"+Telephone);
+
+
+
